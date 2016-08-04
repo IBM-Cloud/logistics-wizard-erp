79822956cfc22a759a112a411773ef92efe8d5f2
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2016-08-04 11:21:37 +0200

    inject demoId of the connected user when looking for the inventories of a distribution center #21

--- common/models/distribution-center.js
+++ common/models/distribution-center.js
@@ -1,12 +1,71 @@
 // Licensed under the Apache License. See footer for details.
 var helper = require("./helper.js");
+var async = require("async");
 
-module.exports = function(DistributionCenter) {
+module.exports = function (DistributionCenter) {
   helper.readOnly(DistributionCenter);
   helper.hideRelation(DistributionCenter, "dcAddress");
   helper.hideRelation(DistributionCenter, "dcContact");
   helper.readOnlyRelation(DistributionCenter, "products");
-  helper.readOnlyRelation(DistributionCenter, "inventories");
+  helper.hideRelation(DistributionCenter, "inventories");
+
+  DistributionCenter.getInventories = function (token, dcId, cb) {
+    async.waterfall([
+      // find the user
+      function (callback) {
+          DistributionCenter.app.models.ERPUser.findById(token.userId, function (err, user) {
+            callback(err, user);
+          });
+      },
+      // find the inventories
+      function (user, callback) {
+          DistributionCenter.app.models.Inventory.find({
+              where: {
+                locationId: dcId,
+                locationType: "DistributionCenter",
+                demoId: user.demoId
+              }
+            },
+            function (err, inventories) {
+              callback(err, inventories);
+            });
+      }
+      ],
+      function (err, inventories) {
+        cb(err, inventories);
+      });
+  }
+
+  DistributionCenter.remoteMethod("getInventories", {
+    description: "Gets inventories at the given distribution center.",
+    http: {
+      verb: "GET",
+      path: "/:id/inventories"
+    },
+    accepts: [
+      {
+        arg: "token",
+        type: "AccessToken",
+        http: function (ctx) {
+          return ctx.req.accessToken;
+        }
+      },
+      {
+        arg: "id",
+        type: "number",
+        required: true,
+        http: {
+          source: "path"
+        }
+      }
+    ],
+    returns: {
+      arg: "data",
+      type: ["Inventory"],
+      root: true
+    }
+  });
+
 };
 //------------------------------------------------------------------------------
 // Licensed under the Apache License, Version 2.0 (the "License");
