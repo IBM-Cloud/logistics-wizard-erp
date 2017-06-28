61ee7e626405582362c56e72eddf0b9fbbfa04a8
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-06-28 16:18:15 +0200

    istioctl

--- .bluemix/pipeline-DEPLOY-Kubernetes.sh
+++ .bluemix/pipeline-DEPLOY-Kubernetes.sh
@@ -22,12 +22,10 @@ bx plugin install container-service -r Bluemix
 figlet -f small 'kubectl'
 wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x /tmp/Bluemix_CLI/bin/kubectl
-kubectl version
 
 figlet -f small 'istioctl'
 curl -L https://git.io/getIstio | sh -
 (cd istio-* && ln -s $PWD/bin/istioctl /tmp/Bluemix_CLI/bin/istioctl)
-istioctl version
 
 figlet 'Logging in Bluemix'
 bx login -a "$CF_TARGET_URL" --apikey "$BLUEMIX_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
@@ -38,11 +36,14 @@ echo -e 'Setting KUBECONFIG...'
 exp=$(bx cs cluster-config $CLUSTER_NAME | grep export)
 eval "$exp"
 
+kubectl version
+istioctl version
+
 echo "Using Docker image $IMAGE_NAME"
 ESCAPED_IMAGE_NAME=$(echo $IMAGE_NAME | sed 's/\//\\\//g')
 cat erp-deployment.yml | sed 's/registry.ng.bluemix.net\/<namespace>\/lw-erp:latest/'$ESCAPED_IMAGE_NAME'/g' > tmp-deployment.yml
 
 echo -e 'Deploying service...'
-istio delete -f lw-erp-routes.yml
-istio create -f lw-erp-routes.yml
+istioctl delete -f lw-erp-routes.yml
+istioctl create -f lw-erp-routes.yml
 kubectl apply -f tmp-deployment.yml
