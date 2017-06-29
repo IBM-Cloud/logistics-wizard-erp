2b87f6e54a9ca39e83092c82853d53c0762d64f6
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-06-29 09:44:09 +0200

    copy needed files to archive directory

--- .bluemix/pipeline-BUILD-Docker.sh
+++ .bluemix/pipeline-BUILD-Docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export FULL_IMAGE_NAME=${REGISTRY_URL}/lw-erp:latest
+export FULL_IMAGE_NAME=${REGISTRY_URL}/${IMAGE_NAME}:latest
 echo "Using Docker image ${FULL_IMAGE_NAME}"
 docker build -t ${FULL_IMAGE_NAME} .
 
-echo "Adding image.env to archive directory..."
+echo "Preparing archive directory..."
 echo "IMAGE_NAME=${FULL_IMAGE_NAME}" > $ARCHIVE_DIR/image.env
+cp -R .bluemix lw-erp*.yml $ARCHIVE_DIR
+ls -lR $ARCHIVE_DIR
