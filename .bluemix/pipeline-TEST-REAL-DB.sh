5fe562bc9857a71e2239bc07dcd715f96f0dac6b
Frederic Lavigne <frederic.lavigne@ibm.com>
2019-11-08 11:12:27 -0600

    bx -> ibmcloud

--- .bluemix/pipeline-TEST-REAL-DB.sh
+++ .bluemix/pipeline-TEST-REAL-DB.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# login to allow test-with-cloudant.sh bx commands
 echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
-bx login -a "$CF_TARGET_URL" --apikey "$IAM_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
+ibmcloud login -a "$CF_TARGET_URL" --apikey "$IAM_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
 
 npm config delete prefix
 nvm install 12.13.0
