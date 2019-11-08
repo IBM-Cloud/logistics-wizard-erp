049e4a321b0b992298032d84de938a556501f227
Frederic Lavigne <frederic.lavigne@ibm.com>
2019-11-08 11:28:06 -0600

    set resource group

--- .bluemix/pipeline-TEST-REAL-DB.sh
+++ .bluemix/pipeline-TEST-REAL-DB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
-ibmcloud login -a "$CF_TARGET_URL" --apikey "$IAM_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
+ibmcloud login -a "$CF_TARGET_URL" --apikey "$IAM_API_KEY" -o "$CF_ORG" -s "$CF_SPACE" -g "$RESOURCE_GROUP"
 
 npm config delete prefix
 nvm install 12.13.0
